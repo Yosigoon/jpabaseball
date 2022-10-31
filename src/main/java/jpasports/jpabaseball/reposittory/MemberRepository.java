e859f271a25a3271857387d8e47d0ec98a0dcd89
hwlee <yosigoon@gmail.com>
2022-10-31 20:39:22 +0900

    jpa member entity repository add

---
+++ src/main/java/jpasports/jpabaseball/reposittory/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpasports.jpabaseball.reposittory;
+
+import jpasports.jpabaseball.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member); //영속성 Context 에  Member 객체 넣고 트랜잭션 커밋 시점에 DB 반영
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)   //JPQL from 대상이 엔티티
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
